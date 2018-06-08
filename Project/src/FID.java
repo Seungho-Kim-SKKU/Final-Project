3403fa3a90eb0fe6e280ec8d732d5272cac62d2f
Kim Seungho <Kim Seungho@DESKTOP-BJ8DAU5>
2018-06-09 01:22:12 +0900

    이상호 나쁜놈

--- Project/src/FID.java
+++ Project/src/FID.java
@@ -8,14 +8,17 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
-public class FID extends JFrame{
+public class FID extends JFrame{	
+	String a = "aaa";
+	
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
+	private JTextField SID;
+	private JTextField Name;
 	
 	public FID() {
 		setVisible(true);
@@ -27,10 +30,10 @@ public class FID extends JFrame{
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		textField = new JTextField();
-		textField.setBounds(145, 50, 258, 33);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		SID = new JTextField();
+		SID.setBounds(145, 50, 258, 33);
+		contentPane.add(SID);
+		SID.setColumns(10);
 		
 		JLabel lblNewLabel = new JLabel("학번 :");
 		lblNewLabel.setFont(new Font("굴림", Font.BOLD, 23));
@@ -42,14 +45,20 @@ public class FID extends JFrame{
 		lblNewLabel_1.setBounds(42, 118, 85, 33);
 		contentPane.add(lblNewLabel_1);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(145, 122, 258, 33);
-		contentPane.add(textField_1);
-		textField_1.setColumns(10);
+		Name = new JTextField();
+		Name.setBounds(145, 122, 258, 33);
+		contentPane.add(Name);
+		Name.setColumns(10);
 		
 		JButton finding = new JButton("ID 확인");
 		finding.setBounds(76, 190, 111, 42);
 		contentPane.add(finding);
+		finding.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Idf();
+			}
+		});
 		
 		JButton cancel = new JButton("취소");
 		cancel.setBounds(222, 190, 111, 42);
@@ -60,5 +69,25 @@ public class FID extends JFrame{
 				dispose();
 			}
 		});
+	}
+	
+	public void Idf() {
+		if(new String(SID.getText()).equals("test") && Name.getText().equals("test")) {
+			setVisible(true);
+			
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			setBounds(100, 100, 259, 186);
+			contentPane = new JPanel();
+			contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+			setContentPane(contentPane);
+			contentPane.setLayout(null);
+			
+			JLabel outputID = new JLabel("당신의 아이디는 " + a + " 입니다.");
+			outputID.setBounds(12, 55, 219, 44);
+			contentPane.add(outputID);						
+		}
+		else {
+            JOptionPane.showMessageDialog(null, "Faild");
+        }
 	}
 }
